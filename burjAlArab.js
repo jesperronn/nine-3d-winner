3772508c9a2835aa34f8141ad1879e6561ef36d0
Jesper Rønn-Jensen <jesperrr@gmail.com>
2015-09-10 15:00:30 +0400

    spir sat på

--- burjAlArab.js
+++ burjAlArab.js
@@ -7,27 +7,37 @@
 function main() {
 
 
+    var firkantSize = 10;
+    var spirSize    = .3;
+    //hotelform flytning med centrum i firkant
+    var halv = firkantSize/2;
+    var firkantFlyt = [-halv, -halv, 0];
+    //hotel spir flyttes den anden vej
+    var spirFlyt = [halv-spirSize,halv-spirSize,0];
 
     // vores standard objekter
-    var firkantSize = 10;
     var firkant = cube({size: [firkantSize,firkantSize,26]})
-                    .translate([-firkantSize/2,-firkantSize/2,0]);
-    
+                    .translate(firkantFlyt);
 
-    //sejlets z-akse skal have centrum i 10 højde (1/3 oppe)
+
+    // sejlets z-akse skal have centrum i 10 højde (1/3 oppe)
     var sejlRadius = 30;
     var sejlet  = sphere(sejlRadius)
                     .translate([0,0,10])
                     .setColor([.2,.2,.9,1])
                     .translate([2/3*sejlRadius,2/3*sejlRadius,0]);
     var klods   = cube({size: [1,1,1]});
-
+    var spir    = cube({size:[spirSize, spirSize,40]})
+                    .translate(spirFlyt)
+                    .setColor(0,1,0);
+                    //.scale(.5);
 
     var sten = [];
     var hotelForm = intersection(firkant,sejlet);
     //sten.push(firkant);
     //sten.push(sejlet)
     sten.push(hotelForm);
+    sten.push(spir);
 
     //sten.push(klods.translate([0,0,0]));
     sten.push(klods.translate([0,10,0]).setColor(1,0,0));
@@ -35,5 +45,5 @@ function main() {
     sten.push(klods.translate([10,0,0]).setColor(0,0,1));
 
     return union(sten);
-    
+
 }
