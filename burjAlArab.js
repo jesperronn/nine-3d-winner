2f8c740e427e54366a411c82fd5371153c3811c3
jesperlyster <jly@nine.dk>
2015-09-10 12:44:01 +0200

    Update burjAlArab.js
    version 2

--- burjAlArab.js
+++ burjAlArab.js
@@ -9,14 +9,17 @@ function main() {
 
 
     // vores standard objekter
-    var firkant = cube({size: [10,10,30]});
+    var firkantSize = 10;
+    var firkant = cube({size: [firkantSize,firkantSize,26]})
+                    .translate([-firkantSize/2,-firkantSize/2,0]);
+    
 
     //sejlets z-akse skal have centrum i 10 højde (1/3 oppe)
-    var sejlRadius = 28;
+    var sejlRadius = 30;
     var sejlet  = sphere(sejlRadius)
                     .translate([0,0,10])
                     .setColor([.2,.2,.9,1])
-                    .translate([sejlRadius,0,0]);
+                    .translate([2/3*sejlRadius,2/3*sejlRadius,0]);
     var klods   = cube({size: [1,1,1]});
 
 
@@ -26,7 +29,11 @@ function main() {
     //sten.push(sejlet)
     sten.push(hotelForm);
 
-    sten.push(klods.translate([30,0,0]));
-    sten.push(klods.translate([30,-20,0]));
+    //sten.push(klods.translate([0,0,0]));
+    sten.push(klods.translate([0,10,0]).setColor(1,0,0));
+    sten.push(klods.translate([0,0,-10]).setColor(0,1,0));
+    sten.push(klods.translate([10,0,0]).setColor(0,0,1));
 
     return union(sten);
+    
+}
